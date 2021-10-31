b7247a5784a3920942c54d461dc85cd09721b488
Matt602 <matthew.frierson062@gmail.com>
2021-10-31 10:42:07 -0500

    added eda_data.csv

---
+++ model_building.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 31 10:25:42 2021
+
+@author: matth
+"""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+df = pd.read_csv('eda_data.csv')
+
